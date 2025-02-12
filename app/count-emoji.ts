031542ed0b6c1e1b7a92a0ba79b8f9a16a53206f
Michael van de Water <36352239+mjvdw@users.noreply.github.com>
2025-02-12 19:55:05 +1300

    Add some things

--- app/count-emoji.ts
+++ app/count-emoji.ts
@@ -1,38 +1,20 @@
-function receiveMessage(e: { [key: string]: any }) {
-    countReactions(e);
-
+function receiveMessage(e: GoogleAppsScript.Events.DoPost) {
+    let message = e.parameter.payload;
+    let reactions = collectReactionsFromMessage(message);
 }
 
 
-function countReactions(message: { [key: string]: any }) {
-    console.log("Counting emoji");
-    console.log("Testing!");
-
-
-    let slack = new Slack();
+function collectReactionsFromMessage(message: { [key: string]: any }) {
+    let slack = new GASU.connectSlack();
     let response = slack.getReactions(message);
 
     if (response.getResponseCode() == 200) {
-        console.log("Response code: " + response.getResponseCode());
         let data = JSON.parse(response.getContentText());
+
+        console.log(data);
+
         let reactions = data.message.reactions;
-        let count = 0;
 
-        for (let i = 0; i < reactions.length; i++) {
-            count += reactions[i].count;
-        }
-
-        let text = "Total reactions: " + count;
-        let url = message.response_url;
-
-        let options: object = {
-            method: "post",
-            payload: {
-                text: text,
-            },
-        };
-
-        let postResponse = UrlFetchApp.fetch(url, options);
-        return postResponse;
+        return reactions;
     }
 };
