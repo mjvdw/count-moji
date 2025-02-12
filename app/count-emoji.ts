0ecc85aaf0c3865a7031ac4bbc16a26df80b67e8
Michael van de Water <36352239+mjvdw@users.noreply.github.com>
2025-02-12 18:05:01 +1300

    Save progress

--- app/count-emoji.ts
+++ app/count-emoji.ts
@@ -1,8 +1,38 @@
-function countEmoji(message) {
-    console.log(message);
-    console.log("Counting emoji.");
+function receiveMessage(e: { [key: string]: any }) {
+    countReactions(e);
+
+}
+
+
+function countReactions(message: { [key: string]: any }) {
+    console.log("Counting emoji");
+    console.log("Testing!");
+
 
     let slack = new Slack();
-    slack.getReactions(message);
+    let response = slack.getReactions(message);
 
+    if (response.getResponseCode() == 200) {
+        console.log("Response code: " + response.getResponseCode());
+        let data = JSON.parse(response.getContentText());
+        let reactions = data.message.reactions;
+        let count = 0;
+
+        for (let i = 0; i < reactions.length; i++) {
+            count += reactions[i].count;
+        }
+
+        let text = "Total reactions: " + count;
+        let url = message.response_url;
+
+        let options: object = {
+            method: "post",
+            payload: {
+                text: text,
+            },
+        };
+
+        let postResponse = UrlFetchApp.fetch(url, options);
+        return postResponse;
+    }
 };
