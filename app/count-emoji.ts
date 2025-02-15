44c3cf3d8c936d8705d6db2f088e6dc3d20831f1
Michael van de Water <36352239+mjvdw@users.noreply.github.com>
2025-02-15 15:48:41 +1300

    Setting up user id to username mapping

--- app/count-emoji.ts
+++ app/count-emoji.ts
@@ -1,23 +1,49 @@
 function receiveMessage(e: GoogleAppsScript.Events.DoPost) {
     console.log("Received message");
-    console.log(JSON.parse(e.parameter.payload).user);
+    // console.log(JSON.parse(e.parameter.payload).user);
     let message = JSON.parse(e.parameter.payload);
-    let reactions = collectReactionsFromMessage(message);
+
+    try {
+        let reactions = collectReactionsFromMessage(message);
+        if (reactions == undefined) {
+            console.log("Error: Could not collect reactions");
+            return;
+        }
+
+        for (let reaction of reactions) {
+            console.log("People: ", reaction.people);
+            // console.log(reaction.emoji + ": " + reaction.people.map((person: { [key: string]: any }) => person.user.name).join(", "));
+        }
+    } catch (e) {
+        console.log("Error: ", e);
+
+    }
 }
 
 
-function collectReactionsFromMessage(message: { [key: string]: any }) {
+function collectReactionsFromMessage(message: { [key: string]: any }): Array<{ [key: string]: any }> | undefined {
     let slack = new GASU.connectSlack();
     let response = slack.getReactions(message);
 
     if (response.getResponseCode() == 200) {
         let data = JSON.parse(response.getContentText());
 
-        console.log(data);
-
         let reactions = data.message.reactions;
 
-        console.log(reactions);
+        reactions = reactions.map((reaction: { [key: string]: any }) => {
+            let emoji = reaction.name;
+
+            let people: Array<{ [key: string]: any }> = [];
+            reaction.users.map((userId: string) => {
+                let person = JSON.parse(slack.getUserById(userId).getContentText())
+                people.push(person);
+            });
+
+            return {
+                emoji: emoji,
+                people: people
+            }
+        });
 
         return reactions;
     }
